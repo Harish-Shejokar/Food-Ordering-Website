7b72992bf0a1bd2df862b48980360c9b4044f59c
Harish-Shejokar <hshejokar@gmail.com>
2023-03-21 15:08:03 +0530

    able to complet task-2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React,{Fragment} from "react";
 import Header from "./Components/Layout/Header";
+import FoodSummary from "./Components/Meals/FoodSummary";
 
 function App() {
   return (
     <Fragment>
       <Header />
+      <FoodSummary />
     </Fragment>
   );
 }
