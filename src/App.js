655dc8c9d69de33d07a72d6e59437fe5bd106435
Harish-Shejokar <hshejokar@gmail.com>
2023-03-23 16:15:07 +0530

    made a hard coded cart to show number of item selected

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React,{Fragment} from "react";
 import Header from "./Components/Layout/Header";
 import Meals from "./Components/Meals/Meals";
+import Cart from './Components/Cart/Cart'
+
 function App() {
   return (
     <Fragment>
+      <Cart/>
       <Header />
       <main>
         <Meals />
       </main>
+      
     </Fragment>
   );
 }
